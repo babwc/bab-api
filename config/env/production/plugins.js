c772321080f9afcd1ac11705db23662bb8b6cb93
babwc <admin@babushkamarket.com>
2023-11-20 20:29:29 -0800

    switched to upload-do

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,22 +10,33 @@ module.exports = ({ env }) => ({
   //     },
   //   },
   // },
+  // upload: {
+  //   config: {
+  //     provider: "aws-s3",
+  //     providerOptions: {
+  //       accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+  //       secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+  //       endpoint: env("DO_SPACE_ENDPOINT"),
+  //       region: "us-west-1",
+  //       params: {
+  //         Bucket: env("DO_SPACE_BUCKET"),
+  //       },
+  //     },
+  //     actionOptions: {
+  //       upload: {},
+  //       uploadStream: {},
+  //       delete: {},
+  //     },
+  //   },
+  // },
   upload: {
     config: {
-      provider: "@strapi/provider-upload-aws-s3",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
-        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
-        region: "us-west-1",
-        params: {
-          Bucket: env("DO_SPACE_BUCKET"),
-        },
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
+        space: env("DO_SPACE_BUCKET"),
       },
     },
   },
