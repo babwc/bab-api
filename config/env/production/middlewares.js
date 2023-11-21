1c5427acd94d14fd09dfe80512dc1ddec91f35c9
babwc <admin@babushkamarket.com>
2023-11-20 21:47:41 -0800

    final switch to upload-do

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,72 +1,37 @@
-// module.exports = [
-//   "strapi::errors",
-//   {
-//     name: "strapi::security",
-//     config: {
-//       contentSecurityPolicy: {
-//         useDefaults: true,
-//         directives: {
-//           "connect-src": ["'self'", "https:"],
-//           "img-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "dl.airtable.com",
-//             "*.digitaloceanspaces.com",
-//           ],
-//           "media-src": [
-//             "'self'",
-//             "data:",
-//             "blob:",
-//             "dl.airtable.com",
-//             "*.digitaloceanspaces.com",
-//           ],
-//           upgradeInsecureRequests: null,
-//         },
-//       },
-//     },
-//   },
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::logger",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
-
-// module.exports = ({ env }) => [
-// "strapi::errors",
-// "strapi::cors",
-// "strapi::poweredBy",
-// "strapi::logger",
-// "strapi::query",
-// "strapi::body",
-// "strapi::favicon",
-// "strapi::public",
-// {
-//   name: "strapi::security",
-//   config: {
-//     contentSecurityPolicy: {
-//       useDefaults: true,
-//       directives: {
-//         "connect-src": ["'self'", "https:"],
-//         "img-src": [
-//           "'self'",
-//           "data:",
-//           "blob:",
-//           `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
-//         ],
-//         "media-src": [
-//           "'self'",
-//           "data:",
-//           "blob:",
-//           `${env("DO_SPACE_BUCKET")}.${env("DO_SPACE_ENDPOINT")}`,
-//         ],
-//         upgradeInsecureRequests: null,
-//       },
-//     },
-//   },
-// },
-// ];
+module.exports = [
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.digitaloceanspaces.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "*.digitaloceanspaces.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
