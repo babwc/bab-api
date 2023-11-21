81930a565a45475871b9b1bf058a236b2bc94411
babwc <admin@babushkamarket.com>
2023-11-20 16:31:03 -0800

    changed upload config

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,12 +1,30 @@
 module.exports = ({ env }) => ({
+  // upload: {
+  //   config: {
+  //     provider: "strapi-provider-upload-dos",
+  //     providerOptions: {
+  //       key: env("DO_SPACE_ACCESS_KEY"),
+  //       secret: env("DO_SPACE_SECRET_KEY"),
+  //       endpoint: env("DO_SPACE_ENDPOINT"),
+  //       space: env("DO_SPACE_BUCKET"),
+  //     },
+  //   },
+  // },
   upload: {
     config: {
-      provider: "strapi-provider-upload-dos",
+      provider: "aws-s3",
       providerOptions: {
-        key: env("DO_SPACE_ACCESS_KEY"),
-        secret: env("DO_SPACE_SECRET_KEY"),
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
-        space: env("DO_SPACE_BUCKET"),
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
