f473919536e053f42e13590899960bffe94d8dc9
babwc <admin@babushkamarket.com>
2023-11-20 20:51:39 -0800

    switched to upload-do

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -10,34 +10,34 @@ module.exports = ({ env }) => ({
   //     },
   //   },
   // },
-  // upload: {
-  //   config: {
-  //     provider: "aws-s3",
-  //     providerOptions: {
-  //       accessKeyId: env("DO_SPACE_ACCESS_KEY"),
-  //       secretAccessKey: env("DO_SPACE_SECRET_KEY"),
-  //       endpoint: env("DO_SPACE_ENDPOINT"),
-  //       region: "us-west-1",
-  //       params: {
-  //         Bucket: env("DO_SPACE_BUCKET"),
-  //       },
-  //     },
-  //     actionOptions: {
-  //       upload: {},
-  //       uploadStream: {},
-  //       delete: {},
-  //     },
-  //   },
-  // },
   upload: {
     config: {
-      provider: "strapi-provider-upload-do",
+      provider: "aws-s3",
       providerOptions: {
-        key: env("DO_SPACE_ACCESS_KEY"),
-        secret: env("DO_SPACE_SECRET_KEY"),
+        accessKeyId: env("DO_SPACE_ACCESS_KEY"),
+        secretAccessKey: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
-        space: env("DO_SPACE_BUCKET"),
+        region: "us-west-1",
+        params: {
+          Bucket: env("DO_SPACE_BUCKET"),
+        },
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
+  // upload: {
+  //   config: {
+  //     provider: "strapi-provider-upload-do",
+  //     providerOptions: {
+  //       key: env("DO_SPACE_ACCESS_KEY"),
+  //       secret: env("DO_SPACE_SECRET_KEY"),
+  //       endpoint: env("DO_SPACE_ENDPOINT"),
+  //       space: env("DO_SPACE_BUCKET"),
+  //     },
+  //   },
+  // },
 });
