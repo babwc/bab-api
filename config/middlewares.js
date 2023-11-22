1d1d64cb9c0234f273115c6c6c2fb3bca44fd1d4
babwc <admin@babushkamarket.com>
2023-11-21 17:33:24 -0800

    edited cors policy temp

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+module.exports = ({ env }) => [
+  "strapi::errors",
+  "strapi::security",
+  "strapi::poweredBy",
+
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: [`${env("CLIENT")}`],
+    },
+  },
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
